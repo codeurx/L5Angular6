7051b80c828fead609bf552ad624815b072310ec
Amir Ali Salah <codeurx@gmail.com>
2018-07-19 13:44:46 +0200

    First commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use JWTAuth;
+use Validator;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        $token = JWTAuth::attempt($credentials);
+        if ($token) {
+            return response()->json(['token' => $token]);
+        } else {
+            return response()->json(['message' => 'Invalid credentials.'], 401);
+        }
+    }
+
+    public function getUser(Request $request)
+    {
+        $user = JWTAuth::parseToken()->authenticate();
+        return response()->json($user);
+    }
+
+    public function logout(Request $request)
+    {
+        $token = JWTAuth::getToken();
+        JWTAuth::invalidate($token);
+        return response()->json(['message' => 'The token has been blacklisted'], 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('/login', [
+    'uses' => 'UserController@login'
+]);
+Route::get('/user', [
+    'uses' => 'UserController@getUser',
+    'middleware' => 'jwt.auth'
+]);
+Route::get('/logout', [
+    'uses' => 'UserController@logout',
+    'middleware' => 'jwt.auth'
+]);
+
